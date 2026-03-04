b28bdf10054b0a5184b3e6b15195b45bdb9eb3b2
kowshik kumar sarker <sarkerkowshik016@gmail.com>
2026-03-04 19:33:04 +0600

    Add files via upload

---
+++ 03_varriable.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# this is my varriable type
+a=10
+name="kowshik"
+age=90
+echo "my name is  $name  and age is  $age"
+
+name="sarker"
+
+echo " my name is  $name "
+
+
+
+#Var to store the  output of a command 
+
+Hostname=$(hostname)
+echo " name of this machine is  $Hostname"
