a58aa8c904b3ee4a365da1e9dfd469fefce1eb99
kowshik kumar sarker <sarkerkowshik016@gmail.com>
2026-03-04 19:35:07 +0600

    Add files via upload

---
+++ 08_userInteraction.sh
@@ -0,0 +1,10 @@
+#normally take input from user
+echo "what is your name? ---"
+read name
+echo "  my name is name $name"
+
+#another way of deaclear
+
+read -p " what is your age ? " age
+echo "my age is $age "
+
