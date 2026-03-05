3446aea1e0afc50643ceef0e7a1090eed6be4610
kowshik kumar sarker <sarkerkowshik016@gmail.com>
2026-03-05 17:41:12 +0600

    Add files via upload

---
+++ rembember.sh
@@ -0,0 +1,13 @@
+pwd
+home/doh
+
+echo $?(checking exist status)
+0 (means sucess)
+
+if command wrong it will give rando number after echo $?
+
+
+echo $$----> give pip id for ps -al
+
+echo $0---> bash/zsh/any other
+
