655e5de32bf84df2ac8e3e1ab7ce5a789c0131c1
kowshik kumar sarker <sarkerkowshik016@gmail.com>
2026-03-10 13:28:49 +0600

    Add files via upload

---
+++ rememeber2.sh
@@ -0,0 +1,18 @@
+
+
+kowshik@Serversite:~/Learn_scripts$ realpath test.csv
+/home/kowshik/Learn_scripts/test.csv
+
+
+kowshik@Serversite:~/Learn_scripts$ basename /home/kowshik/Learn_scripts/test.csv
+test.csv
+
+
+same use dirname will give full path of any file
+
+
+>cd /root/ &> /dev/null
+...this willvanished our output of programme.
+
+
+echo "the name of  the script is :echo ${0}"
