45b33d4f7efa58e9da5d204f8f03c7b89bd4ec51
kowshik kumar sarker <sarkerkowshik016@gmail.com>
2026-03-10 13:34:11 +0600

    Add files via upload

---
+++ 38_debuging.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#set -x ..will give step by step process how we need to work
+
+#set -e gives exits from scripts ...where the  commnad fail and did not write any of things
+
+
+set -e
+pwd
+date
+cd /root
+hostname
