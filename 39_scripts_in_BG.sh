67a75f04592e8409f59f80f64f5622ee4030d3f0
kowshik kumar sarker <sarkerkowshik016@gmail.com>
2026-03-10 13:34:34 +0600

    Add files via upload

---
+++ 39_scripts_in_BG.sh
@@ -0,0 +1,19 @@
+#!//bin/bash
+
+#running scripts in bg
+
+
+for i in  {1..18}
+do 
+
+	echo " number is $i"
+	sleep 3s
+done
+
+
+#another loop
+for name in kowshik riya nomi
+do
+	echo "name is  $name"
+done
+
